4a32094ac3744125a382430e4b9763601ccab3e3
Kartikkp07 <80201555+Kartikkp07@users.noreply.github.com>
2021-08-08 13:37:11 +0530

    Update games.js

--- games.js
+++ games.js
@@ -47,7 +47,7 @@ function playSound(name){
 
 // ***************************************************************************************************
 // storing user response
-$(".btn").click(function(){
+$(".btn").on('click touchstart', (function(){
   var userChosenColour=$(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
